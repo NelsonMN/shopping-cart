5ef6e044f3be25c3704b82acf9abb7eb0e8974a1
Nelson <nelson.mieszkalski@gmail.com>
2022-06-23 16:06:02 -0400

    Added tent images and edited data structure

--- src/data/tents.js
+++ src/data/tents.js
@@ -1,16 +1,101 @@
+import tent1 from "../images/tents/tent1.jpg";
+import tent2 from "../images/tents/tent2.jpg";
+import tent3 from "../images/tents/tent3.jpg";
+import tent4 from "../images/tents/tent4.jpg";
+import tent5 from "../images/tents/tent5.jpg";
+import tent6 from "../images/tents/tent6.jpg";
+import tent7 from "../images/tents/tent7.jpg";
+import tent8 from "../images/tents/tent8.jpg";
+import tent9 from "../images/tents/tent9.jpg";
+import tent10 from "../images/tents/tent10.jpg";
+import tent11 from "../images/tents/tent11.jpg";
+import tent12 from "../images/tents/tent12.jpg";
+
 const tents = [
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
-  { category: "tents", id: "", name: "", price: "", image: "" },
+  {
+    category: "tents",
+    id: "tent1",
+    name: "Base Camper 6-Person Tent",
+    price: 469.95,
+    image: `${tent1}`,
+  },
+  {
+    category: "tents",
+    id: "tent2",
+    name: "Spark 2.0 3-Person Tent",
+    price: 519.95,
+    image: `${tent2}`,
+  },
+  {
+    category: "tents",
+    id: "tent3",
+    name: "Wawona 6-Person Tent",
+    price: 699.99,
+    image: `${tent3}`,
+  },
+  {
+    category: "tents",
+    id: "tent4",
+    name: "Super Dome 4-Person Tent",
+    price: 459.99,
+    image: `${tent4}`,
+  },
+  {
+    category: "tents",
+    id: "tent5",
+    name: "Aurora 3-Person Tent",
+    price: 459.95,
+    image: `${tent5}`,
+  },
+  {
+    category: "tents",
+    id: "tent6",
+    name: "Midori Solo 1-Person Tent",
+    price: 179.95,
+    image: `${tent6}`,
+  },
+  {
+    category: "tents",
+    id: "tent7",
+    name: "Midori 3-Person Tent",
+    price: 319.95,
+    image: `${tent7}`,
+  },
+  {
+    category: "tents",
+    id: "tent8",
+    name: "Hubba Hubba 3-Person Tent",
+    price: 699.95,
+    image: `${tent8}`,
+  },
+  {
+    category: "tents",
+    id: "tent9",
+    name: "Suma 3-Person Tent",
+    price: 249.95,
+    image: `${tent9}`,
+  },
+  {
+    category: "tents",
+    id: "tent10",
+    name: "El Capitan 4-Person Tent",
+    price: 479.95,
+    image: `${tent10}`,
+  },
+  {
+    category: "tents",
+    id: "tent11",
+    name: "Camper 2-Person Tent",
+    price: 279.95,
+    image: `${tent11}`,
+  },
+  {
+    category: "tents",
+    id: "tent12",
+    name: "Cabin 2.0 4-Person Tent",
+    price: 549.95,
+    image: `${tent12}`,
+  },
 ];
 
 export default tents;
