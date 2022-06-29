cbe116bab765baaec519e2ae8ff686e684f51639
Nelson <nelson.mieszkalski@gmail.com>
2022-06-29 11:27:38 -0400

    Added cart stylings and remove button

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
-import '../styles/cart-page.css'
+import '../styles/cart-page.css';
+import '../images/credit-card-logos.png';
 
 const Cart = ({ numItems, setNumItems, cart, setCart, total, setTotal }) => {
 
@@ -27,14 +28,13 @@ const NonEmptyCart = ({ numItems, setNumItems, cart, setCart, total, setTotal })
     <div className='nonempty-cart-page'>
       <h1>My Cart ({numItems} items)</h1>
       <div className='cart-items'>
-        <CartHolder cart={cart} setCart={setCart} setNumItems={setNumItems} setTotal={setTotal} />
-        <Total total={total} />
+        <CartHolder cart={cart} setCart={setCart} setNumItems={setNumItems} total={total} setTotal={setTotal} />
       </div>
     </div>
   )
 }
 
-const CartHolder = ({ cart, setCart, setNumItems, setTotal }) => {
+const CartHolder = ({ cart, setCart, setNumItems, total, setTotal }) => {
   const incrementItem = (element) => {
     element.quantity += 1;
     const cartClone = [...cart];
@@ -44,12 +44,14 @@ const CartHolder = ({ cart, setCart, setNumItems, setTotal }) => {
   }
 
   const decrementItem = (element) => {
-    if (element.quantity > 0) {
+    if (element.quantity > 1) {
       element.quantity -= 1;
       const cartClone = [...cart];
       setCart(cartClone)
       setNumItems(n => n - 1)
       setTotal(t => t - element.item.price)
+    } else if (element.quantity === 1) {
+      removeItem(element)
     }
   }
 
@@ -64,31 +66,47 @@ const CartHolder = ({ cart, setCart, setNumItems, setTotal }) => {
 
   return (
     <div className='cart-item-holder'>
-    {cart.map(element => { 
-      return (
-        <div className='cart-item' key={element.id}>
-          <img src={element.item.image} alt={element.item.name}></img>
-          <div className='cart-item-details'>
-            <h3>{element.item.name}</h3>
-            <h3>${element.item.price} CAD</h3>
-            <button className='remove-item' onClick={() => removeItem(element)}>Remove Item</button>
+      {cart.map(element => { 
+        return (
+          <div className='cart-item' key={element.id}>
+            <Link className='cart-link' to={`/shop/${element.id}`}><img className='cart-img' src={element.item.image} alt={element.item.name}></img></Link>
+            <div className='cart-item-details'>
+              <h3>{element.item.name}</h3>
+              <h3>${element.item.price} CAD</h3>
+              <button className='remove-item' onClick={() => removeItem(element)}>Remove Item</button>
+            </div>
+            <div className="counter">
+              <button onClick={() => decrementItem(element)}>-</button>
+              <div className="count">{element.quantity}</div>
+              <button onClick={() => incrementItem(element)}>+</button>
+            </div>
           </div>
-          <div className="counter">
-            <button onClick={() => decrementItem(element)}>-</button>
-            <div className="count">{element.quantity}</div>
-            <button onClick={() => incrementItem(element)}>+</button>
-          </div>
-        </div>
-        )
-      })}
+          )
+        })}
+      <Total total={total} />
+      <button className='check-out'>Check Out</button>
+      <img src='../src/images/credit-card-logos.png' alt='credit-card-logos'></img>
     </div>
   ) 
 }
 
 const Total = ({ total }) => {
   return (
-    <div className='cart-item'>
-      <h1>Total: ${total} CAD</h1>
+    <div className='total'>
+      <h1>Total</h1>
+      <div className='sub-total'>
+        <h2>Sub-total</h2>
+        <h2>${total}</h2>
+      </div>
+      <div className='sub-total'>
+        <h2>Shipping</h2>
+        <h2>FREE</h2>
+      </div>
+      <div className='sub-total'>
+        <h2>Total (HST/GST incl.)</h2>
+        <h2>${total} CAD</h2>
+      </div>
+      
     </div>
   )
 }
